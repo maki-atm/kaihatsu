fabf72f338bc95d51689e8970371b27737c114a4
id <id@IN0013451>
2021-07-15 14:20:15 +0900

    カテゴリー機能（セッションを使用）

--- src/main/java/com/example/demo/CategoryRepository.java
+++ src/main/java/com/example/demo/CategoryRepository.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+	List<Category> findByCategoryName(String categoryName);
 
 
 }
